202006c8c5c13ff4a0e6967c3859b11ab84762a1
justnotiyann <muhammadfitrian0712@gmail.com>
2022-07-19 18:13:40 +0700

    add card icon and function style

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,46 +1,35 @@
-var button = document.querySelector('#search')
-var input = document.querySelector('.value')
-var result = document.querySelector('.result')
+// Tangkap Elemen form lalu tampung kedalam variabel
+let getInput  = document.querySelector('#input')
+let getButton = document.querySelector('#search')
 
-// Middleware haha
-function cek(m) {
-  console.log(m)
+
+getButton.addEventListener('click', function () {
+  // Tangkap value pada input
+  let value = getInput.value
+  // nilai value akan selalu huruf kecil
+  let data = value.toLowerCase()
+  // Eksekusi
+  getFetch(data)
+})
+
+// Function Fetch
+function getFetch(data) {
+    let api = `https://pokeapi.co/api/v2/pokemon/${data}`
+    fetch(api)
+      .then(res => res.json())
+      .then(res => {
+        // Panggil function getDatas()
+        getDatas(res)
+      })
 }
 
-button.addEventListener('click', function () {
-    // Fetch Data
-    fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`)
-        .then(res => res.json())
-        .then(res => {
-            cek(input.value) // Belajar middleware, jadi setiap pada sebuah proses dapat kita akses
-            result.innerHTML =
-              `
-              <div class="row">
-                <div class="col-12">
-                  <table class="table">
-                  <p class="fw-bold text-center">Tabel Data Pokemon</p>
-                      <thead>
-                        <tr>
-                          <th scope='col'>Gambar</th>
-                          <th scope="col">ID Pokemon</th>
-                          <th scope="col">Nama</th>
-                          <th scope="col">Tinggi</th>
-                          <th scope="col">Berat</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td><img src="${res.sprites.front_default}" alt="Pokemon" class="text-center img-thumbnail rounded-circle" /></td>
-                          <td>${res.id}</td>
-                          <td>${res.name}</td>
-                          <td>${res.height} cm</td>
-                          <td>${res.weight} kg</td>
-                        </tr>
-                      </tbody>
-                    </table>
-              </div>
-            </div>
-              `
-                
-        })
-})
+// getDatas
+function getDatas(res) {
+  const namaPokemon = res.name.toUpperCase()
+  const imgPokemon = res.sprites.front_default
+  const ability = res.abilities[0].ability.name
+  console.log(namaPokemon, imgPokemon, ability)
+  document.querySelector('#gambarPokemon').src = imgPokemon
+  document.querySelector('.card-title').innerText = namaPokemon
+  document.querySelector('.ability').innerText = ability
+}
